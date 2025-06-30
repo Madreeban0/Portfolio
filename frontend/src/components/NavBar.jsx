95aa00d55c27fe621b75cd2b9162c55bbb13277b
Madreeban0 <madreeban277@gmail.com>
2025-06-30 11:07:37 +0530

    Added margin

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-scroll';
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-full h-25 bg-gradient-to-r from-gray-600 via-pink-500 to-orange-700 text-black shadow z-50">
+    <nav className="fixed w-full h-25 bg-gradient-to-r from-blue-600 via-pink-500  text-black shadow z-50">
     <div className="container mx-auto flex justify-between items-center p-4">
-    <h1 className="text-l mr-90 font-bold">Reeban Austrive</h1>
+    <h1 className="text-4xl mr-90 font-bold">Reeban Austrive</h1>
     <div className="space-x-4">
       <Link to="home" smooth duration={500}>Home</Link>
       <Link to="about" smooth duration={500}>About</Link>
